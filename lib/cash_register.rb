0fe6d66e23a7ad34c5c4f7d1d9e93136716886e8
Sarah Rogers <s.rogers0715@yahoo.com>
2020-08-02 19:27:43 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
+class CashRegister
+  attr_accessor :total, :discount
+  def initialize( discount = 20)
+    @total = 0
+    @discount = discount
+  end 
+  
+  def add_item(title, price, quanity=1 )
+    @total+= price * quanity
+  end
 
+
+  def apply_discount
+    percentage = (1.0 - (0.01 * @discount))
+   self.total = (@total * percentage)
+     "After the discount, the total comes to $#{self.total.to_i}."
+end
+
+    # def void_last_transaction
+    # end
+
+
+end
