0a5f0fbee4b35b12085f0c629545acbdf67ddaf6
Sarah Rogers <s.rogers0715@yahoo.com>
2020-08-02 19:53:06 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,8 +12,7 @@ class CashRegister
 
   def apply_discount
     percentage = (1.0 - (0.01 * @discount))
-  # self.total = (@total * percentage)
-     "After the discount, the total comes to $#{self.total.to_i}."
+  
 end
 
     # def void_last_transaction
